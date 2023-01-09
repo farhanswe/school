58813b5cde61ba01450dbd9a3288c885d36b046c
farhanswe <farhanswe35@gmail.com>
2023-01-09 13:00:18 +0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+
+{
+    'name': "School",
+
+    'summary': """
+    School
+    """,
+
+    'description': """
+        School
+    """,
+
+    'author': "Farhan",
+    'version': '15.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+
+    # always loaded
+    'data': [
+    ],
+    # only loaded in demonstration mode
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "license": "AGPL-3"
+}
